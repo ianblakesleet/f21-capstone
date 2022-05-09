9291ec3ad54fe9fb0f3addcff530ad2e9ea034f7
ianblakesleet <ianblakesleet@gmail.com>
2022-05-08 21:00:11 -0600

    attempt 5

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 const app = express()
 const { SERVER_PORT } = process.env
 app.use(cors())
